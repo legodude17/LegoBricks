8c4ae1b622af849d1f398676f7609417e5ed4c4d
Joshua <legodude17@users.noreply.github.com>
2021-02-19 02:52:44 -0800

    Add settings library

---
+++ Source/Settings/SettingsRenderer.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace Settings
+{
+    [StaticConstructorOnStartup]
+    public class SettingsRenderer
+    {
+        public static bool __DEBUG = false;
+
+        private static readonly Dictionary<Type, Type> CustomDrawers = new Dictionary<Type, Type>();
+
+        private readonly Dictionary<string, string> buffers = new Dictionary<string, string>();
+
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        internal readonly object Settings;
+        private string curTab = "";
+        private Dictionary<FieldInfo, ICustomRenderer> customFields;
+        private FieldInfo[] drawFields;
+        private Dictionary<string, FieldInfo> fieldInfos;
+        private string[] keys;
+        private Listing_Standard listingStandard;
+        private string mainTabName;
+        public bool Ready;
+        private Dictionary<string, SettingsRenderer> renderers;
+        private Vector2 scrollPos = new Vector2(0, 0);
+        private bool setRect;
+        internal Dictionary<FieldInfo, MethodInfo> Setters;
+        private SettingsRenderer[] subSettings;
+        private List<TabRecord> tabs;
+        private string title;
+        private Rect viewRect;
+
+        static SettingsRenderer()
+        {
+            AddCustomDrawer(typeof(Def), typeof(CustomDefDrawer));
+        }
+
+        public SettingsRenderer(object settings, string name = "")
+        {
+            Settings = settings;
+            title = name;
+        }
+
+        private float Height => keys.Length * 20f + subSettings.Sum(ss => ss.Height);
+
+        public static void AddCustomDrawer(Type drawee, Type drawer)
+        {
+            if (!typeof(ICustomRenderer).IsAssignableFrom(drawer))
+            {
+                Log.Error(
+                    "[LegoBricks] [Settings] Tried to register custom drawer that does not implement required interface.");
+                return;
+            }
+
+            CustomDrawers.Add(drawee, drawer);
+        }
+
+        public void Init()
+        {
+            if (Settings is INamedSettings ns) title = ns.Name;
+            var type = Settings.GetType();
+            Debug("Settings is of type: " + type.Name);
+            if (Settings is IEnumerable<object> list)
+            {
+                Debug("Settings is Enumerable!");
+                var objects = list as object[] ?? list.ToArray();
+                var settingsAndNames =
+                    objects.OfType<INamedSettings>().Select(obj => new Pair<object, string>(obj, obj.Name)).Concat(
+                        objects.Where(obj => obj.GetType().HasAttribute<SettingAttribute>()).Select(obj =>
+                            new Pair<object, string>(obj, obj.GetType().TryGetAttribute<SettingAttribute>()?.Name)));
+                var srs = settingsAndNames.Select(pair => new SettingsRenderer(pair.First, pair.Second))
+                    .ToArray();
+                Debug("Created " + srs.Length + " renderers!");
+                tabs = srs.Select(sr =>
+                    new TabRecord(sr.title, () => curTab = sr.title, false)
+                    {
+                        selectedGetter = () => curTab == sr.title
+                    }).ToList();
+                Debug("Created " + tabs.Count + " tabs!");
+                renderers = tabs.Select(tab => tab.label)
+                    .ToDictionary(srs);
+                Debug("Created dictionary!");
+                mainTabName = title.NullOrEmpty() ? "Main" : title;
+                var mainTab = new TabRecord(mainTabName, () => curTab = mainTabName, true)
+                {
+                    selectedGetter = () => curTab == mainTabName
+                };
+                curTab = mainTabName;
+                Debug("Created mainTab!");
+                tabs.Insert(0, mainTab);
+                Debug("Inserted main tab!");
+                foreach (var sr in srs)
+                {
+                    Debug("Initializing tab!");
+                    sr.Init();
+                }
+            }
+            else
+            {
+                mainTabName = title.NullOrEmpty() ? "Main" : title;
+                curTab = mainTabName;
+            }
+
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+            Setters = fields.Where(info => info.HasAttribute<SettingsSetterAttribute>()).ToDictionary(info => info,
+                info => type.GetMethod(info.TryGetAttribute<SettingsSetterAttribute>().MethodName,
+                    BindingFlags.Public | BindingFlags.NonPublic |
+                    BindingFlags.Instance | BindingFlags.Static));
+            Debug("Found " + fields.Length + " fields");
+            foreach (var fieldInfo in fields)
+                Debug("  " + fieldInfo.Name + " (" + fieldInfo.FieldType.Name + ")");
+            fieldInfos = fields.ToDictionary(field => field.Name, field => field);
+            Debug("Created types dictionary!");
+            drawFields = fields.Where(field => typeof(ICustomSettingsDraw).IsAssignableFrom(field.FieldType))
+                .ToArray();
+            Debug("Found " + drawFields.Length + " drawFields");
+            fields = fields.Except(drawFields).ToArray();
+            var valueFields = fields.Where(field =>
+                field.FieldType.IsValueType || field.FieldType.IsEnum || typeof(string).IsAssignableFrom(
+                    field.FieldType)).ToArray();
+            Debug("Found " + valueFields.Length + " valueFields");
+            fields = fields.Except(valueFields).ToArray();
+            var classFields =
+                fields.Where(field => field.FieldType.IsClass && !field.FieldType.IsEnum).ToArray();
+            customFields = classFields
+                .Where(info => CustomDrawers.Keys.Any(type1 => type1.IsAssignableFrom(info.FieldType))).ToDictionary(
+                    info => info, info => (ICustomRenderer) Activator.CreateInstance(CustomDrawers.First(kv =>
+                        kv.Key.IsAssignableFrom(info.FieldType)).Value));
+            Debug("Found " + customFields.Count + " custom drawing fields");
+            subSettings = classFields.Except(customFields.Keys)
+                .Select(field => new SettingsRenderer(field.GetValue(Settings), field.Name))
+                .ToArray();
+            Debug("Found " + subSettings.Length + " subSettings");
+            foreach (var setting in subSettings)
+            {
+                Debug("Initializing sub setting");
+                setting.Init();
+            }
+
+            fields = fields.Except(valueFields).Except(classFields).ToArray();
+            fields.ToList().ForEach(field =>
+                Log.Error(field.Name + " is not a class or value! Type is " + field.FieldType.Name));
+            keys = valueFields.Select(field => field.Name).ToArray();
+            listingStandard = new Listing_Standard();
+            Ready = true;
+        }
+
+        public void Render(Rect inRect)
+        {
+            if (!Ready) Init();
+            RenderInternal(inRect, listingStandard);
+        }
+
+        private void RenderInternal(Rect inRect, Listing_Standard listing)
+        {
+            if (tabs != null) TabDrawer.DrawTabs(inRect, tabs);
+            Debug("curTab is " + curTab + " and ready is " + Ready);
+            if (curTab == mainTabName)
+            {
+                if (!setRect)
+                {
+                    viewRect = inRect.AtZero();
+                    setRect = true;
+                }
+
+                listing.BeginScrollView(inRect, ref scrollPos, ref viewRect);
+                Debug("Begun scroll view");
+
+                foreach (var field in drawFields)
+                    if (field.GetValue(Settings) is ICustomSettingsDraw icsd)
+                        icsd.Render(listing.GetRect(icsd.Height), listing);
+                    else
+                        Log.ErrorOnce("Field " + field.Name + " is not ICustomSettingsDraw", field.Name.GetHashCode());
+
+                Debug("Finished drawing custom fields");
+
+                foreach (var key in keys) RenderValue(key, listing);
+
+                Debug("Finished drawing keys");
+
+                foreach (var subSetting in subSettings)
+                {
+                    var listing2 = listing.BeginSection_NewTemp(subSetting.Height);
+                    var rect = listing.GetRect(subSetting.Height);
+                    if (!subSetting.title.NullOrEmpty())
+                    {
+                        Debug("Drawing subSettings " + subSetting.title);
+                        Text.Font = GameFont.Medium;
+                        listing2.Label(subSetting.title);
+                        Text.Font = GameFont.Small;
+                    }
+
+                    subSetting.RenderInternal(rect, listing2);
+                    listing.EndSection(listing2);
+                }
+
+                Debug("Finished drawing subSettings");
+
+                foreach (var kv in customFields)
+                    kv.Value.Render(SettingLabel(kv.Key.Name, kv.Key), Tooltip(kv.Key.Name, kv.Key), kv.Key, Settings,
+                        listing, this);
+
+                Debug("Finished drawing custom renderers");
+
+                listing.EndScrollView(ref inRect);
+            }
+            else
+            {
+                renderers[curTab].RenderInternal(inRect, listing);
+            }
+        }
+
+        private void RenderValue(string key, Listing_Standard listing)
+        {
+            Debug("Rendering value " + key);
+            var info = fieldInfos[key];
+            Debug("Found info: " + info.Name + " on " + info.DeclaringType?.Name + " in " +
+                  info.DeclaringType?.Namespace);
+            var type = info.FieldType;
+            var label = SettingLabel(key, info);
+            var curValue = info.GetValue(Settings);
+            if (type.IsEnum)
+            {
+                var names = type.GetEnumNames();
+                foreach (var name in names) Debug("Found name: " + name);
+                var values = names.ToDictionary(type.GetEnumValues().OfType<object>());
+                foreach (var kv in values) Debug(kv.Key + " is of type " + kv.Value);
+                if (listing.ButtonTextLabeled(label, curValue.ToString()))
+                    Find.WindowStack.Add(new FloatMenu(names.Select(name =>
+                        new FloatMenuOption(name, () => info.SetValue(Settings, values[name], this))).ToList()));
+            }
+
+            if (type == typeof(string))
+            {
+                Debug("Found string!");
+                info.SetValue(Settings, listing.TextEntryLabeled(label, curValue?.ToString() ?? ""), this);
+            }
+            else if (type == typeof(bool))
+            {
+                Debug("Found bool!");
+                var temp = (bool) curValue;
+                listing.CheckboxLabeled(label, ref temp, Tooltip(key, info));
+                info.SetValue(Settings, temp, this);
+            }
+            else if (type == typeof(int))
+            {
+                Debug("Found int!");
+                var num = (int) curValue;
+                buffers.TryGetValue(key, out var buffer);
+                buffer = buffer ?? "";
+                listing.TextFieldNumericLabeled(label, ref num, ref buffer);
+                info.SetValue(Settings, num, this);
+                buffers.SetOrAdd(key, buffer);
+            }
+            else if (type == typeof(float))
+            {
+                Debug("Found float!");
+                var num = (float) curValue;
+                buffers.TryGetValue(key, out var buffer);
+                buffer = buffer ?? "";
+                listing.TextFieldNumericLabeled(label, ref num, ref buffer);
+                info.SetValue(Settings, num, this);
+                buffers.SetOrAdd(key, buffer);
+            }
+        }
+
+        private string SettingLabel(string key, FieldInfo info)
+        {
+            if (Settings is ISettingsLabeler sl) return sl.GetLabel(key);
+            if (info.TryGetAttribute(out SettingAttribute attr) && !attr.Label.NullOrEmpty()) return attr.Label;
+            return key;
+        }
+
+        private string Tooltip(string key, FieldInfo info)
+        {
+            if (Settings is ISettingsTooltipper st) return st.GetTooltip(key);
+            if (info.TryGetAttribute(out SettingAttribute attr) && !attr.Tooltip.NullOrEmpty()) return attr.Tooltip;
+            return null;
+        }
+
+        private static void Debug(string message)
+        {
+            if (__DEBUG) Log.Message(message);
+        }
+    }
+
+    public interface ICustomSettingsDraw
+    {
+        float Height { get; }
+        void Render(Rect inRect, Listing_Standard listing);
+    }
+
+    public interface INamedSettings
+    {
+        string Name { get; }
+    }
+
+    public interface ISettingsLabeler
+    {
+        string GetLabel(string key);
+    }
+
+    public interface ISettingsTooltipper
+    {
+        string GetTooltip(string key);
+    }
+
+    public static class Util
+    {
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, TValue>> source)
+        {
+            return source.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            this IEnumerable<TKey> source, IEnumerable<TValue> other)
+        {
+            return source.Zip(other, (key, value) => new KeyValuePair<TKey, TValue>(key, value))
+                .ToDictionary();
+        }
+
+        public static void SetValue(this FieldInfo info, object obj, object val, SettingsRenderer renderer = null)
+        {
+            if (renderer != null && renderer.Setters.ContainsKey(info))
+                renderer.Setters[info].Invoke(renderer.Settings, new[] {obj, info, val});
+            else
+                info.SetValue(obj, val);
+        }
+    }
+
+    public class SettingAttribute : Attribute
+    {
+        public string Label;
+        public string Name;
+        public string Tooltip;
+
+        public SettingAttribute(string label, string tooltip = null)
+        {
+            Label = label;
+            Tooltip = tooltip;
+        }
+
+        public SettingAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public SettingAttribute(string label = "", string tooltip = "", string name = "")
+        {
+            Label = label;
+            Tooltip = tooltip;
+            Name = name;
+        }
+    }
+
+    public class SettingsSetterAttribute : Attribute
+    {
+        public string MethodName;
+
+        public SettingsSetterAttribute(string methodName)
+        {
+            MethodName = methodName;
+        }
+    }
+
+    public interface ICustomRenderer
+    {
+        void Render(string label, string tooltip, FieldInfo info, object obj, Listing_Standard listing,
+            SettingsRenderer renderer);
+    }
+
+    public class CustomDefDrawer : ICustomRenderer
+    {
+        private Def curDef;
+        private Vector2 scrollPos = new Vector2(0, 0);
+        private string searchBar = "";
+        private bool setRect;
+        private Rect viewRect;
+
+        public void Render(string label, string tooltip, FieldInfo info, object obj, Listing_Standard listing,
+            SettingsRenderer renderer)
+        {
+            curDef = (Def) info.GetValue(obj);
+            var rect = listing.GetRect(300f);
+            Widgets.Label(rect.LeftHalf(), label);
+            var rect1 = rect.RightHalf();
+            searchBar = Widgets.TextField(rect1.TopPartPixels(30f), searchBar);
+            var defs = GenDefDatabase.GetAllDefsInDatabaseForDef(info.FieldType);
+            var listing1 = new Listing_Standard();
+            var rect2 = rect1.BottomPartPixels(270f);
+            if (!setRect)
+            {
+                viewRect = rect2.AtZero();
+                setRect = true;
+            }
+
+            listing1.BeginScrollView(rect2, ref scrollPos, ref viewRect);
+            foreach (var def1 in defs.Where(def =>
+                def.label.Contains(searchBar) || def.description.Contains(searchBar) ||
+                def.defName.Contains(searchBar)))
+            {
+                var rect4 = listing1.GetRect(20f);
+                if (Widgets.ButtonText(rect4, def1.label))
+                {
+                    searchBar = "";
+                    curDef = def1 == curDef ? null : def1;
+                    info.SetValue(obj, curDef, renderer);
+                }
+
+                TooltipHandler.TipRegion(rect4, def1.description);
+
+                if (def1 == curDef)
+                {
+                    GUI.color = Color.yellow;
+                    Widgets.DrawBox(rect4, 2);
+                    GUI.color = Color.white;
+                }
+            }
+
+            listing1.EndScrollView(ref viewRect);
+            TooltipHandler.TipRegion(rect.LeftHalf(), tooltip);
+        }
+    }
+}
---
+++ Source/Settings/TestMod.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace Settings
+{
+    // public class TestMod : Mod
+    // {
+    //     private readonly SettingsRenderer renderer;
+    //     public TestSettings Settings;
+    //
+    //     public TestMod(ModContentPack content) : base(content)
+    //     {
+    //         SettingsRenderer.__DEBUG = true;
+    //         Settings = GetSettings<TestSettings>();
+    //         renderer = new SettingsRenderer(Settings);
+    //     }
+    //
+    //     public override string SettingsCategory()
+    //     {
+    //         return "Test 1";
+    //     }
+    //
+    //     public override void DoSettingsWindowContents(Rect inRect)
+    //     {
+    //         base.DoSettingsWindowContents(inRect);
+    //         renderer.Render(inRect);
+    //     }
+    // }
+
+    public class TestSettings : ModSettings, ISettingsLabeler, ISettingsTooltipper, IEnumerable<TestSettingsTab>
+    {
+        public string CustomStory;
+        public bool Enabled;
+        public int Number;
+        public float Number2;
+
+        private List<TestSettingsTab> tabs = new List<TestSettingsTab>();
+
+        public IEnumerator<TestSettingsTab> GetEnumerator()
+        {
+            return tabs.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public string GetLabel(string key)
+        {
+            switch (key)
+            {
+                case "Enabled":
+                    return "Enable Feature?";
+                case "CustomStory":
+                    return "Your custom story!";
+                case "Number1":
+                    return "The first number:";
+                case "Number2":
+                    return "The second number:";
+            }
+
+            return key;
+        }
+
+        public string GetTooltip(string key)
+        {
+            switch (key)
+            {
+                case "Enabled":
+                    return "Feature does things x, y, and z.";
+            }
+
+            return null;
+        }
+
+        public void LogTabs()
+        {
+            foreach (var tab in tabs) tab.Log();
+        }
+
+        public void InitTabs()
+        {
+            if (tabs.NullOrEmpty())
+                tabs = new List<TestSettingsTab>
+                {
+                    new TestSettingsTab(1), new TestSettingsTab(2), new TestSettingsTab(3), new TestSettingsTab(4)
+                };
+            foreach (var tab in tabs)
+                while (tabs.Except(tab).Any(tab2 => tab.Index == tab2.Index))
+                    tab.Index++;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref Enabled, "Enabled", true);
+            Scribe_Values.Look(ref CustomStory, "CustomStory", "Hello this is the default");
+            Scribe_Values.Look(ref Number, "Number");
+            Scribe_Values.Look(ref Number2, "Number2");
+            Scribe_Collections.Look(ref tabs, "tabs", LookMode.Deep);
+            InitTabs();
+            LogTabs();
+        }
+    }
+
+    public class TestSettingsTab : ISettingsLabeler, ISettingsTooltipper, INamedSettings, IExposable
+    {
+        public bool Enabled;
+        internal int Index;
+        public int Number;
+        public float Number2;
+
+        public TestSettingsTab()
+        {
+        }
+
+        public TestSettingsTab(int idx)
+        {
+            Index = idx;
+        }
+
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref Enabled, "Enabled");
+            Scribe_Values.Look(ref Number, "Number");
+            Scribe_Values.Look(ref Number2, "Number2");
+            Scribe_Values.Look(ref Index, "Index");
+        }
+
+        public string Name => "Feature " + Index;
+
+        public string GetLabel(string key)
+        {
+            switch (key)
+            {
+                case "Enabled":
+                    return "Enable " + Name + "?";
+                case "Number":
+                    return "First number for this feature!";
+                case "Number2":
+                    return "Your custom story!";
+            }
+
+            return key;
+        }
+
+        public string GetTooltip(string key)
+        {
+            switch (key)
+            {
+                case "Enabled":
+                    return Name + " does things a, b, and c.";
+            }
+
+            return null;
+        }
+
+        public void Log()
+        {
+            Verse.Log.Message("tab " + Index + " is " + (Enabled ? "Enabled" : "Not Enabled"));
+        }
+    }
+}
